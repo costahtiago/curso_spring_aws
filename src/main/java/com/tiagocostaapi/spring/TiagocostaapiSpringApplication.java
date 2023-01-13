b4eaf75ab1edf33f8155af3dbe93a12269d1e80a
Thiago Henrique da Costa <thiago.henrique@eletrosom.local>
2023-01-12 23:48:11 -0300

    subindo curso spring + aws

---
+++ src/main/java/com/tiagocostaapi/spring/TiagocostaapiSpringApplication.java
@@ -0,0 +1,13 @@
+package com.tiagocostaapi.spring;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TiagocostaapiSpringApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TiagocostaapiSpringApplication.class, args);
+	}
+
+}
